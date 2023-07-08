b50ef5e91fa9f54a0453771fe9b4bc7c39edf4fd
Vinayak Chittora <vinayakchittora@gmail.com>
2023-07-08 11:46:45 +0530

    changes done

--- socket/index.js
+++ socket/index.js
@@ -2,7 +2,7 @@ import {Server} from 'socket.io';
 
 const io = new Server(9000, {
     cors: {
-        origin: "https://master--soft-croquembouche-eef5c8.netlify.app"
+        origin: "http://localhost:3000"
     }
 })
 
